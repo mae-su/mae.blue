795347353ac44638cbadc82cb116c49b96267800
mae-su@glider <50283719+mae-su@users.noreply.github.com>
2023-10-22 18:57:35 -0400

    yeah

--- post.py
+++ post.py
@@ -6,19 +6,18 @@ with open('debug.html', 'r', encoding='utf-8') as f:
 
 soup = BeautifulSoup(debug_content, 'html.parser')
 
+def dump_stream(id):
+    stream_div = soup.find('div', {'id': id})
+    stream_children_content = ''.join(str(child) for child in stream_div.children)
 
-stream_div = soup.find('div', {'id': 'stream'})
-stream_content = str(stream_div.prettify())
-stream_children_content = ''.join(str(child) for child in stream_div.children)
+    print(f'{id} div fetched. Dumping to ./content/{id}.html')
+    with open(f'./content/{id}.html', 'w', encoding='utf-8') as f:
+        f.write(stream_children_content)
+    print(f'{id} children dumped successfully.')
 
-print('Stream div fetched. Dumping to ./content/stream.html')
-with open('./content/stream.html', 'w', encoding='utf-8') as f:
-    f.write(stream_children_content)
-print('Stream children dumped successfully.')
-
-for child in stream_div.children:
-    child.replaceWith('')
-print('Cleared stream div children. Dumping.')
+    for child in stream_div.children:
+        child.replaceWith('')
+    print(f'Cleared {id} div children.')
 with open('index.html', 'w', encoding='utf-8') as f:
     f.write(str(soup))
 
